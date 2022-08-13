5acee1c7b02efd6312e9fe4881288a1507244903
javierlara97 <javila.com@hotmail.com>
2022-08-13 13:15:14 -0500

    Creación de vista, formularios y rutas

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+Use App\Models\Empleado;
 use Illuminate\Http\Request;
 
 class EmpleadoController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('empleados.index');
+    }
+
+    public function create()
+    {
+        return view('empleados.create');
+    }
+
+    public function store(Request $request)
+    {
+        $datosEmpleados = request()->all();
+        return response()->json($datosEmpleados);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadoController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('/empleados', function () {
+    return view('empleados.index');
+});
+
+Route::get('/empleados/create', [EmpleadoController::class,'create']);
+*/
+
+Route::resource('empleados', EmpleadoController::class);
